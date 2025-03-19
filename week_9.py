071eb06906e7ecea6107f54e1bd43fbba1f9d7a1
Ryan Kang <ryankdy@berkeley.edu>
2025-03-20 07:49:38 +0900

    Update week_9.py

--- week_9.py
+++ week_9.py
@@ -13,8 +13,7 @@ try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(16, GPIO.IN)
     GPIO.add_event_detect(16, GPIO.BOTH, callback=my_callback)
- 
-    message = raw_input('\nPress any key to exit.\n')
+
  
 finally:
     GPIO.cleanup()
